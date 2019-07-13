7e85296bcfe194d58f28af2e2c992cbfaede1864
Arka-h <47897466+Arka-h@users.noreply.github.com>
2019-07-13 18:52:18 +0000

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded',()=>{
+	document.querySelector('button').onclick = function(){
+		this.style="opactity: 0";
+	};
+})
+	// var name
+	// function hello(){
+	// 	name = document.querySelector('#name').value;
+	// 	console.log(name);
+	// 	alert(`Hello ${name} !`);
+	// }
+	// document.querySelector('h1').innerHTML=`Hello ${name} !`;
+	// 
